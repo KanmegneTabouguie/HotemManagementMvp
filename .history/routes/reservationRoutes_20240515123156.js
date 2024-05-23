0387a462413400556ea403095a48dcdba705d6db
kanmegne <andrelaurelyvan.kanmegnetabouguie@ynov.com>
2024-05-24 00:40:57 +0200

    final version both backend and frontedn

--- .history/routes/reservationRoutes_20240515123156.js
+++
@@ -1,9 +0,0 @@
-// routes/reservationRoutes.js
-const express = require('express');
-const router = express.Router();
-const reservationController = require('../controllers/reservationController');
-
-// Create a new reservation
-router.post('/reservations', reservationController.createReservation);
-
-module.exports = router;
